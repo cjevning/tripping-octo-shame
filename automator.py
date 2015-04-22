8988b03e0bee2e1a4d25ff5b800fa9b16855fd71
Jordan <jordans@stanford.edu>
2015-04-21 22:32:27 -0700

    2nd commit

--- automator.py
+++ automator.py
@@ -2,4 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("www.youtube.com")
+
+def youtubeSearcher(song_name):
+	driver.get("http://www.youtube.com")
+	searchBar = driver.find_element_by_id("masthead-search-term")
+	searchBar.send_keys(song_name)
+	searchBar.send_keys(Keys.RETURN)
+
+	driver.close()

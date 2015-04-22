adfcf766341fcf02258cc56aac6f0abc7faea854
Conner Jevning <cjevning@Conners-MacBook-Air.local>
2015-04-21 22:29:22 -0700

    file to get added

--- automator.py
+++ automator.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+fileToGet = "https://www.youtube.com/watch?v=_FBwVRNOL70"
 driver = webdriver.Chrome()
 driver.get("www.youtube.com")
